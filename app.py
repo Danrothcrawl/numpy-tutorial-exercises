6f77d1579fa801cf6480fa01c648ef3efb25e04e
Danrothcrawl <dquitze@gmail.com>
2025-04-22 02:16:02 +0000

    mean

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
+# import numpy as np
+
+# print(np.__version__)
+# print(np.show_config())
+# print(np.zeros(10))
+# zeros = np.zeros(10)
+# mem_size = zeros.itemsize * zeros.size
+# print(mem_size)
+# np.info(np.add)
+# arr = np.zeros(10)
+# arr[4] = 1
+# print(arr)
+# arr = np.arange(10,50)
+# print(arr)S
+# arr = np.arange(0,10)
+# print(arr[::-1])
+# arr = np.arange(0,9)
+# print(np.reshape(arr, (3,3)))
+# l = [1,2,0,0,4,0]
+# arr = np.array(l)
+# print(np.nonzero(arr))
+# print(np.eye(3))
+# arr = np.random.random(3)
+# print(arr)
+# arr = np.random.random(10)
+# print(arr)
+# print(np.max(arr))
+# arr = np.random.random(10)
+# print(arr.mean())
 import numpy as np
 
-print(np.__version__)
-print(np.show_config())
-print(np.zeros(10))
-zeros = np.zeros(10)
-mem_size = zeros.itemsize * zeros.size
-print(mem_size)
-np.info(np.add)
-arr = np.zeros(10)
-arr[4] = 1
-print(arr)
-arr = np.arange(10,50)
-print(arr)
-arr = np.arange(0,10)
-print(arr[::-1])
-arr = np.arange(0,9)
-print(np.reshape(arr, (3,3)))
-l = [1,2,0,0,4,0]
-arr = np.array(l)
-print(np.nonzero(arr))
-print(np.eye(3))
-arr = np.random.random(3)
-print(arr)
 arr = np.random.random(10)
-print(arr)
-print(np.max(arr))
-arr = np.random.random(10)
-print(arr)
 print(arr.mean())
